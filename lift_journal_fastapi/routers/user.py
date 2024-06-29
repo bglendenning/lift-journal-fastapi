de75fcd88f0cef05a170fbc45a501bbb66241259
Brandan Glendenning <notbrandan@gmail.com>
2024-06-29 03:52:52 -0500

    Create user in create_user route

--- lift_journal_fastapi/routers/user.py
+++ lift_journal_fastapi/routers/user.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Response, status
+from lift_journal_data.crud import create_user
 from lift_journal_data.schemas import requests
 
 router = APIRouter()
 
 
-@router.post("/user/register", status_code=status.HTTP_202_ACCEPTED)
-def register(user: requests.UserRegister):
+@router.post("/user/create", status_code=status.HTTP_202_ACCEPTED)
+def create_user(user: requests.UserCreate):
+    user = create_user(user)
+
     return Response(status_code=status.HTTP_202_ACCEPTED)
