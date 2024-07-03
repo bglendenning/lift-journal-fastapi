51956c4c6ddd30a54d8494e51f8604ac90587f9e
Brandan Glendenning <notbrandan@gmail.com>
2024-07-03 01:09:51 -0500

    Refactor for lift-journal-data 0.6.0
    Set and set references now have Lift/lift_ prefix.

--- lift_journal_fastapi/routers/set.py
+++ lift_journal_fastapi/routers/set.py
@@ -1,9 +1,9 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
-from lift_journal_data.crud import SetDAO
+from lift_journal_data.crud import LiftSetDAO
 from lift_journal_data.schemas.user import UserReadSchema
-from lift_journal_data.schemas.set import SetBaseSchema, SetCreateSchema
+from lift_journal_data.schemas.lift_set import LiftSetBaseSchema, LiftSetCreateSchema
 
 from lift_journal_fastapi import db
 from lift_journal_fastapi.authentication import get_token_user
@@ -12,8 +12,8 @@ router = APIRouter()
 
 
 @router.post("/")
-def post_set(lift_set: SetBaseSchema, user: Annotated[UserReadSchema, Depends(get_token_user)]):
-    lift_set = SetCreateSchema(
+def post_set(lift_set: LiftSetBaseSchema, user: Annotated[UserReadSchema, Depends(get_token_user)]):
+    lift_set = LiftSetCreateSchema(
         user_id=user.id,
         lift_id=lift_set.lift_id,
         repetitions=lift_set.repetitions,
@@ -21,7 +21,7 @@ def post_set(lift_set: SetBaseSchema, user: Annotated[UserReadSchema, Depends(ge
         date_performed=lift_set.date_performed,
         time_performed=lift_set.time_performed,
     )
-    db_set = SetDAO(db.SessionLocal()).create(lift_set)
+    db_lift_set = LiftSetDAO(db.SessionLocal()).create(lift_set)
 
-    if db_set:
-        return SetBaseSchema.from_orm(db_set)
+    if db_lift_set:
+        return LiftSetBaseSchema.from_orm(db_lift_set)
